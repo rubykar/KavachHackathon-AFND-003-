488a9ec9e76647255d54df97582d5997447e44c1
KUNDAN SINGH <kundansingh@KUNDANs-MacBook-Air.local>
2023-04-04 19:46:41 +0530

    added app.py 1

--- KAVACHHACKATHON2023/app.py
+++ KAVACHHACKATHON2023/app.py
@@ -1,18 +1,21 @@
 import joblib
 import pickle
 import streamlit as st
+import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 st.set_page_config(page_title = "Fake news detector",page_icon = ":tada:", layout = "wide")
 tfidf_vectorizer = pickle.load(open('tfidf.pkl', 'rb'))
 pickled_model = pickle.load(open('lr_model.pkl', 'rb'))
+knn_model = pickle.load(open('knn_model.pkl', 'rb'))
+data = pd.read_csv("IFND.csv", encoding="ISO-8859-1")
 new_data = ["Abhinav Kashyap claims Salman Khan "]
 new_data_tfidf = tfidf_vectorizer.transform(new_data)
 predicted_label = pickled_model.predict(new_data_tfidf)
 # st.markdown("""<form>
-#       <label for="name">Enter the news:</label>
-#       <input type="text" id="name" name="name" placeholder="Your name...">
-#       <input type="submit" value="Submit">
-#   </form>""", unsafe_allow_html = True)
+# 		<label for="name">Enter the news:</label>
+# 		<input type="text" id="name" name="name" placeholder="Your name...">
+# 		<input type="submit" value="Submit">
+# 	</form>""", unsafe_allow_html = True)
 st.title("Fake news Detector AI")
 st.subheader("BEWARE OF FAKE NEWS!")
 st.subheader("Hello, I am here to help you identify fake news floating around.")
@@ -24,8 +27,23 @@ if news:
     predicted_label = pickled_model.predict(new_data_tfidf)
     #st.write(f"Hello, {name}!")
     if predicted_label[0]=="Fake":
-        st.write("FAKE NEWS DETECTED!")
-        true_news = "True news goes here!"
-        st.write("The true version of this news might be:", true_news)
+            st.write("🚨🚨🚨FAKE NEWS DETECTED!🚨🚨🚨")
+            distances, indices = knn_model.kneighbors(new_data_tfidf)
+
+            # Retrieve the corresponding true news articles that are closest to the new article and have Label == True
+            suggestions = data.loc[indices[0]][(
+                data['Label'] == 'TRUE')]['Statement']
+
+            # Print the suggestions
+            if len(suggestions) > 0:
+                st.write('Suggested true news articles:')
+                j=1
+                for i, suggestion in suggestions.iteritems():
+                    st.write(f"{j}.{suggestion}")
+                    j+=1
+                    st.write(f"   Web value: {data.loc[i]['Web']}")
+            else:
+                st.write('No true news articles found in suggestions.')
+
     else:
         st.write("This is a true news!.")
